89f6323558bdaad0b27673dfa774b1cd7ed415e8
Andrew T <andrewtgit@gmail.com>
2022-12-21 20:28:01 +1100

    Created initial GUI.

--- script.js
+++ script.js
@@ -27,10 +27,31 @@ container.setAttribute('class', 'container')
 document.body.appendChild(container)
 
 const outputBox = document.createElement('div')
-outputBox.setAttribute('id', 'ouput-box')
+outputBox.setAttribute('id', 'output-box')
+outputBox.textContent = 12903812390
 container.appendChild(outputBox)
 
 const btnContainer = document.createElement('div')
 btnContainer.setAttribute('id', 'btn-container')
 container.appendChild(btnContainer)
 
+for (i = 0; i <= 9; i++) {
+    const btn = document.createElement('button')
+    btn.setAttribute('id', `btn-${[i]}`)
+    btn.textContent = i
+    btnContainer.appendChild(btn)
+}
+
+const btnClear = document.createElement('button')
+btnClear.setAttribute('id', 'btn-clear')
+btnClear.textContent = 'C'
+btnContainer.appendChild(btnClear)
+
+const btnEqual = document.createElement('button')
+btnEqual.setAttribute('id', 'btn-equal')
+btnEqual.textContent = '='
+btnContainer.appendChild(btnEqual)
+
+const btn0 = document.getElementById('btn-0')
+const btnC = document.getElementById('btn-clear')
+btnC.after(btn0)
